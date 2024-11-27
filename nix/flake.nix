60c3d856842fc3b461b240e5aaf500fb57241898
Kaia <186507889+kaialist@users.noreply.github.com>
2024-11-27 19:58:23 +0100

    Add files via upload

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,27 +1,43 @@
 {
   description = "NixConfig";
 
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      lib = nixpkgs.lib;
-    in {
-      nixosConfigurations = {
-        nix = lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ 
-            ./configuration.nix
-            modules/nvidia.nix
-            modules/kernel.nix
-            modules/locale.nix
-            modules/packages.nix
-            modules/plasma.nix
-            modules/security.nix
-          ];
+
+  outputs = inputs@{ nixpkgs, home-manager, plasma-manager, ... }: {
+    nixosConfigurations = {
+      nix = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          modules/plasma.nix
+          modules/kernel.nix
+          modules/locale.nix
+          modules/nvidia.nix
+          modules/packages.nix
+          modules/security.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+            home-manager.users.kaia = import ./home.nix;
+          }
+
+        ];
       };
     };
   };
+
 }
