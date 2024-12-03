1e5f779b8e4ce441468942394bff8071345066fb
Kaia <186507889+kaialist@users.noreply.github.com>
2024-12-03 19:06:01 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,15 +6,6 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
 
@@ -24,7 +15,7 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          modules/plasma.nix
+          modules/gnome.nix
           modules/kernel.nix
           modules/locale.nix
           modules/nvidia.nix
@@ -34,7 +25,6 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
             home-manager.users.kaia = import ./home.nix;
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
